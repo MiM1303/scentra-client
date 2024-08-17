0555a640541876d6996d447f9ddc5d6db52873f7
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-08-18 05:19:27 +0600

    added search by name

--- src/pages/Home/ProductsPagination.jsx
+++ src/pages/Home/ProductsPagination.jsx
@@ -72,7 +72,29 @@ const ProductsPagination = () => {
 
     return (
         <div>
-            
+            {/* SEARCH BY TAGS */}
+            <div className="w-1/4 flex flex-row gap-5 pl-0 lg:pl-16 md:mx-auto">
+                    <form onSubmit={handleSearch} className="flex gap-1 mb-10 mx-auto">
+                        <div className="form-control">
+                            <label className="input input-bordered w-fit flex items-center gap-4">
+                                <input onChange={ e =>{
+                                                    setSearchText(e.target.value);
+                                                    // console.log(e.target.value);
+                                                }} 
+                                        type="text" 
+                                        name="search" 
+                                        className="grow" 
+                                        placeholder="Search products by tags" />
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
+                            </label>
+                        </div>
+
+                        <div className="form-control">
+                            {/* <Link to={`/all-products?page=${currentPage}&size=${itemsPerPage}/search/${searchText}`}><button className="btn bg-[#b6f8e4] text-black hover:bg-[#98fbdd] ">Search</button></Link> */}
+                            <button onClick={()=>{setSearchLoad(searchLoad+1)}} className="btn bg-[#b6f8e4] px-2 w-fit text-black hover:bg-[#98fbdd] ">Search</button>
+                        </div>
+                    </form>
+
 
             </div>
 
